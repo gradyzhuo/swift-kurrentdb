08ae7b588e3761a9ae53d86ab8280f8e863010a7
Grady Zhuo <gradyzhuo@gmail.com>
2026-02-14 03:00:39 +0800

    [REFACTOR] Rename ProjectionTarget to ProjectionsTarget for consistency
    * Rename `ProjectionTarget` protocol to `ProjectionsTarget` to follow naming conventions
    * Rename file `ProjectionTarget.swift` to `ProjectionsTarget.swift`
    * Update all protocol references from `ProjectionTarget` to `ProjectionsTarget`
    * Update conformances in AnyTarget, NameTarget, ContinuousTarget, OneTimeTarget, and TransientTarget
    * Update `AnyProjectionTarget` to `AnyProjectionsTarget` for consistency
    * Update `Projections<Target>` generic constraint to use `ProjectionsTarget`
    * Update `KurrentDBClient.projections(of:)` generic constraint to use `ProjectionsTarget`
    * Update documentation and code examples to use `ProjectionsTarget`
    * All 50 tests pass successfully (including 9 ProjectionsTests)

---
+++ Sources/KurrentDB/Projections/Protocols/NameTarget.swift
@@ -0,0 +1,34 @@
+//
+//  NameTarget.swift
+//  KurrentDB-Swift
+//
+//  Created by Grady Zhuo on 2026/2/13.
+//
+
+
+
+public struct NameTarget: ProjectionsTarget, ProjectionControlable {
+    public enum Predefined: String, Sendable {
+        /// Represents the `$by_category` system projection.
+        case byCategory = "$by_category"
+        /// Represents the `$by_correlation_id` system projection.
+        case byCorrelationId = "$by_correlation_id"
+        /// Represents the `$by_event_type` system projection.
+        case byEventType = "$by_event_type"
+        /// Represents the `$stream_by_category` system projection.
+        case streamByCategory = "$stream_by_category"
+        /// Represents the `$streams` system projection.
+        case streams = "$streams"
+    }
+    
+    public let name: String
+    
+    public init(name: String){
+        self.name = name
+    }
+    
+    public init(predefined: Predefined){
+        self.name = predefined.rawValue
+    }
+
+}
--- Sources/KurrentDB/Projections/Protocols/ProjectionTarget+OneTime.swift
+++ Sources/KurrentDB/Projections/Protocols/ProjectionTarget+OneTime.swift
@@ -1,5 +1,5 @@
 //
-//  ProjectionTarget+OneTime.swift
+//  ProjectionsTarget+OneTime.swift
 //  KurrentDB-Swift
 //
 //  Created by Grady Zhuo on 2026/2/5.
@@ -8,13 +8,4 @@
 /// A generic target representing one time projections.
 ///
 /// `OneTimeTarget` is used to perform operations on all projections, with the behavior determined
-public struct OneTimeTarget: ProjectionTarget { }
-
-/// Extension providing static methods to create `ProjectionStream` instances.
-extension ProjectionTarget where Self == OneTimeTarget {
-    public static var onetime: OneTimeTarget{
-        get{
-            .init()
-        }
-    }
-}
+public struct OneTimeTarget: ProjectionsTarget { }
